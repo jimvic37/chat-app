a95e58d27bfe13f97ef2abeea3d1831192a225b1
nedned1108 <rpmshockwave@gmail.com>
2023-10-24 15:12:49 -0400

    added user model

--- backend/src/main/java/com/jump/backend/model/User.java
+++ backend/src/main/java/com/jump/backend/model/User.java
@@ -1,5 +1,69 @@
 package com.jump.backend.model;
 
-public class User {
 
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(unique = true, nullable = false)
+	@NotBlank
+	private String username;
+	
+	@Column(nullable = false)
+	@NotBlank
+	private String password;
+
+	public User() {
+	}
+
+	public User(Integer id, @NotBlank String username, @NotBlank String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
+	
 }
