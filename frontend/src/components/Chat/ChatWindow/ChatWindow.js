dda710edc5f576413fd937fab371a82f45f99e5c
jimvic37@github.com <Ethan>
2023-11-01 11:53:35 -0500

    message funcitionality implemented

--- frontend/src/components/Chat/ChatWindow/ChatWindow.js
+++ frontend/src/components/Chat/ChatWindow/ChatWindow.js
@@ -142,76 +142,47 @@ const ChatWindow = ({
                 className="d-flex justify-content-between mb-4"
               >
                 {message.user.id === userInfo.id ? ( // Check if the message is from the current user
-                  // Render the current user's message on the right
                   <>
                     <div className="pt-1 ms-auto"> 
-                    <MDBCard className="mask-custom">
-                      <MDBCardHeader
-                        className="d-flex justify-content-between p-3"
-                        style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
-                      >
-                        <p className="fw-bold mb-0">{message.user.username}</p>
-                        <p className="text-light small mb-0">
-                          <MDBIcon far icon="clock" /> {momentServices(message.created)}
-                        </p>
-                      </MDBCardHeader>
-                      <MDBCardBody>
-                        <p className="mb-0">{message.content}</p>
-                      </MDBCardBody>
-                    </MDBCard>
+                      <MDBCard className="mask-custom">
+                        <MDBCardHeader
+                          className="d-flex justify-content-between p-3"
+                          style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
+                        >
+                          <p className="fw-bold mb-0">{message.user.username}</p>
+                          <p className="text-light small mb-0">
+                            <MDBIcon far icon="clock" /> {momentServices(message.created)}
+                          </p>
+                        </MDBCardHeader>
+                        <MDBCardBody>
+                          <p className="mb-0">{message.content}</p>
+                        </MDBCardBody>
+                      </MDBCard>
                     </div>
                   </>
                 ) : (
                   // Render other users' messages on the left
                   <>
                     <div className="pt-1 me-auto"> 
-                    <MDBCard className="mask-custom">
-                      <MDBCardHeader
-                        className="d-flex justify-content-between p-3"
-                        style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
-                      >
-                        <p className="fw-bold mb-0">{message.user.username}</p>
-                        <p className="text-light small mb-0">
-                          <MDBIcon far icon="clock" /> {momentServices(message.created)}
-                        </p>
-                      </MDBCardHeader>
-                      <MDBCardBody>
-                        <p className="mb-0">{message.content}</p>
-                      </MDBCardBody>
-                    </MDBCard>
+                      <MDBCard className="mask-custom">
+                        <MDBCardHeader
+                          className="d-flex justify-content-between p-3"
+                          style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
+                        >
+                          <p className="fw-bold mb-0">{message.user.username}</p>
+                          <p className="text-light small mb-0">
+                            <MDBIcon far icon="clock" /> {momentServices(message.created)}
+                          </p>
+                        </MDBCardHeader>
+                        <MDBCardBody>
+                          <p className="mb-0">{message.content}</p>
+                        </MDBCardBody>
+                      </MDBCard>
                     </div>
                   </>
                 )}
               </li>
             ))}
-        {/* <MDBTypography listUnStyled className="text-white">
-          {currentChatMessages.map((message) => (
-            <li
-              key={message.id}
-              className="d-flex justify-content-between mb-4"
-            >
-              <img
-                src={message.user.profile || "https://static.thenounproject.com/png/4530368-200.png"}
-                alt="avatar"
-                className="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
-                width="60"
-              />
-              <MDBCard className="mask-custom">
-                <MDBCardHeader
-                  className="d-flex justify-content-between p-3"
-                  style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
-                >
-                  <p className="fw-bold mb-0">{message.user.username}</p>
-                  <p className="text-light small mb-0">
-                    <MDBIcon far icon="clock" /> {momentServices(message.created)}
-                  </p>
-                </MDBCardHeader>
-                <MDBCardBody>
-                  <p className="mb-0">{message.content}</p>
-                </MDBCardBody>
-              </MDBCard>
-            </li>
-          ))} */}
             <li className="mb-3">
               <MDBTextArea
                 label="Message"
