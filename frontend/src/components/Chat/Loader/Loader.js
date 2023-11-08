45d5c38c246d059777f129f4c5be87b8a5911bd0
Dominic Swaby <domswaby@gmail.com>
2023-11-08 12:21:24 -0500

    extract mui styles to their own file

---
+++ frontend/src/components/Chat/Loader/Loader.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div class="loader">
+      <div class="loader-inner">
+        <div class="loader-block"></div>
+        <div class="loader-block"></div>
+        <div class="loader-block"></div>
+        <div class="loader-block"></div>
+        <div class="loader-block"></div>
+        <div class="loader-block"></div>
+        <div class="loader-block"></div>
+        <div class="loader-block"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
